d473c816612b8ce4ab9d596fbe78c2566d230b0c
Anirudh soni <64551845+HeyAnirudh@users.noreply.github.com>
2021-07-09 22:55:31 +0530

    scripts

--- trail.py
+++ trail.py
@@ -15,13 +15,11 @@ fontScale = 1
 
 def annotate(name):
     certi = cv2.imread("C:\\Users\\Admin\Desktop\\github\\certificates_streamlit\\pics\\certificate.jpg")
-    original = cv2.putText(certi, name, (380, 300), font, fontScale, (0, 0, 0), thickness=2)
+    original = cv2.putText(certi, name, (340, 300), font, fontScale, (0, 0, 0), thickness=2)
     cv2.imwrite("download.png",original)
     st.image(original, caption=None, width=350, use_column_width=None, clamp=False, channels='BGR',
                  output_format='PNG')
 
 
 na=st.text_input('Enter your name')
-if na:
-    annotate(na
-        st.download_buttom
+annotate(na)
