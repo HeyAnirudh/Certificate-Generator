27e33a6b9f0d90b7712568c90a4d7db378985f89
Anirudh soni <64551845+HeyAnirudh@users.noreply.github.com>
2021-07-10 22:25:11 +0530

    lafda

--- trail.py
+++ trail.py
@@ -1,4 +1,6 @@
 import cv2
+import os
+import base64
 import streamlit as st
 import numpy
 import sys
@@ -8,17 +10,26 @@ fontScale = 1
 activities = ["Get certificate","About"]
 choice=st.sidebar.selectbox("Select Activty",activities)
 
-
+#def get_binary_file_downloader_html(bin_file, file_label='File'):
+    #with open(bin_file, 'rb') as f:
+    #    data = f.read()
+    #bin_str = base64.b64encode(data).decode()
+    #href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
+    #return href
 def annotate(name):
     certi = cv2.imread("C:\\Users\\Admin\Desktop\\github\\certificates_streamlit\\pics\\certificate.jpg")
     original = cv2.putText(certi, name, (380, 300), font, fontScale, (0, 0, 0), thickness=2)
-    cv2.imwrite("download.png", original)
-    st.image(original, caption=None, width=350, use_column_width=None, clamp=False, channels='BGR', output_format='PNG')
-
+    #cv2.imwrite("download.png", original)
+    #st.image(original, caption=None, width=350, use_column_width=None, clamp=False, channels='BGR', output_format='PNG')
+    if st.button("Download"):
+        st.image(original, caption=None, width=350, use_column_width=None, clamp=False, channels='BGR',
+                 output_format='PNG')
 if choice =="Get certificate":
+    st.title("Get Your Certificate")
     img=cv2.imread('C:\\Users\\Admin\Desktop\\github\\certificates_streamlit\\pics\\apricus.png')
     na=st.text_input('Enter your name')
     annotate(na)
+    #st.markdown(get_binary_file_downloader_html(original, 'Picture'), unsafe_allow_html=True)
 
 
 if choice =="About":
