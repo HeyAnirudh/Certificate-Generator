703db94369972d19304199db631ab833a6609566
Anirudh soni <64551845+HeyAnirudh@users.noreply.github.com>
2021-07-10 21:16:04 +0530

    lmao

--- trail.py
+++ trail.py
@@ -5,21 +5,24 @@ import sys
 from PIL import Image, ImageDraw
 font = cv2.FONT_HERSHEY_SIMPLEX
 fontScale = 1
-#name=st.text_input('Enter your name')
-#certi=cv2.imread("C:\\Users\\Admin\Desktop\\github\\certificates_streamlit\\pics\\certificate.jpg")
-#original=cv2.putText(certi,name,(380, 300),font,fontScale,(0,0,0),thickness=2)
-
-#cv2.imshow('fun',original)
-#st.image(original,caption=None, width=350, use_column_width=None, clamp=False, channels='BGR', output_format='PNG')
+activities = ["Get certificate","About"]
+choice=st.sidebar.selectbox("Select Activty",activities)
 
 
 def annotate(name):
     certi = cv2.imread("C:\\Users\\Admin\Desktop\\github\\certificates_streamlit\\pics\\certificate.jpg")
-    original = cv2.putText(certi, name, (340, 300), font, fontScale, (0, 0, 0), thickness=2)
-    cv2.imwrite("download.png",original)
-    st.image(original, caption=None, width=350, use_column_width=None, clamp=False, channels='BGR',
-                 output_format='PNG')
+    original = cv2.putText(certi, name, (380, 300), font, fontScale, (0, 0, 0), thickness=2)
+    cv2.imwrite("download.png", original)
+    st.image(original, caption=None, width=350, use_column_width=None, clamp=False, channels='BGR', output_format='PNG')
+
+if choice =="Get certificate":
+    img=cv2.imread('C:\\Users\\Admin\Desktop\\github\\certificates_streamlit\\pics\\apricus.png')
+    na=st.text_input('Enter your name')
+    annotate(na)
 
 
-na=st.text_input('Enter your name')
-annotate(na)
+if choice =="About":
+    st.subheader("Cerficate App")
+    st.markdown("</> with ❤ by Apricus" )
+    st.markdown("follow us on instagram 😃 (https://instagram.com/apricus_vjit?utm_medium=copy_link)")
+
