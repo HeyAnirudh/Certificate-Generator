95dd5e06199319083675c7ada05904f4e576aade
Anirudh soni <64551845+HeyAnirudh@users.noreply.github.com>
2021-07-10 23:18:56 +0530

    koimit

--- certificate.py
+++ certificate.py
@@ -1,29 +1,41 @@
 import cv2
-from PIL import Image, ImageDraw, ImageFont
+import os
+import base64
 import streamlit as st
-import cv2 as cv
+import numpy
+import sys
+from PIL import Image, ImageDraw
 font = cv2.FONT_HERSHEY_SIMPLEX
 fontScale = 1
+activities = ["Get certificate","About"]
+choice=st.sidebar.selectbox("Select Activty",activities)
 
-image = Image.open("/pics/certificate.jpg")
-img=cv.imread('C:\\Users\\Admin\Desktop\\github\\certificates_streamlit\\pics\\apricus.png')
-st.image(img,caption=None, width=350, use_column_width=None, clamp=False, channels='BGR', output_format='PNG')
-st.title("Get your certificates")
-name=st.text_input('Enter your name')
-#original=cv2.putText(certi,name,(50, 50),font,fontScale,(0,0,0),thickness=2)
-#roll=cv.imshow("sample.jpg",original)
-#st.image(original,caption=None, width=350, use_column_width=None, clamp=False, channels='BGR', output_format='PNG')
-
-
-
-def annotate(image,name):
-    draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype("arial.ttf", 20, encoding="unic")
-    original = draw.text((10, 10), text=u'{}'.format(name), font=font)
-    st.image(original, caption=None, width=350, use_column_width=None, clamp=False, channels='BGR', output_format='PNG')
-    return image
-
-st.image(annotate(image,name),caption=None, width=350, use_column_width=None, clamp=False, channels='BGR', output_format='PNG')
+def get_binary_file_downloader_html(bin_file, file_label='File'):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    bin_str = base64.b64encode(data).decode()
+    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
+    return href
+def annotate(name):
+    certi = cv2.imread("C:\\Users\\Admin\Desktop\\github\\certificates_streamlit\\pics\\certificate.jpg")
+    original = cv2.putText(certi, name, (380, 300),font,   fontScale, (0, 0, 0), thickness=2)
+    cv2.imwrite("original.jpg",original)
 
 
+    if st.button("View certificate"):
+        st.image(original, caption=None, width=350, use_column_width=None, clamp=False, channels='BGR',output_format='PNG')
+        st.markdown(get_binary_file_downloader_html('original.jpg', 'Certificate'), unsafe_allow_html=True)
+
+if choice =="Get certificate":
+    st.title("Get Your Certificate")
+    img=cv2.imread('C:\\Users\\Admin\Desktop\\github\\certificates_streamlit\\pics\\apricus.png')
+    na=st.text_input('Enter your name')
+    annotate(na)
+    #st.markdown(get_binary_file_downloader_html(original, 'Picture'), unsafe_allow_html=True)
+
+
+if choice =="About":
+    st.subheader("Cerficate App")
+    st.markdown("</> with ❤ by Apricus" )
+    st.markdown("follow us on instagram 😃 (https://instagram.com/apricus_vjit?utm_medium=copy_link)")
 
