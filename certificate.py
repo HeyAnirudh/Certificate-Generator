54f259259c13942a02e38ac6ca95ed13b6789cb2
Anirudh soni <64551845+HeyAnirudh@users.noreply.github.com>
2021-07-11 09:54:24 +0530

    commit

--- certificate.py
+++ certificate.py
@@ -6,9 +6,11 @@ import numpy
 import sys
 from PIL import Image, ImageDraw
 font = cv2.FONT_HERSHEY_SIMPLEX
-fontScale = 1
+fontScale = 3
 activities = ["Get certificate","About"]
+st.sidebar.image("pics//apricus.png")
 choice=st.sidebar.selectbox("Select Activty",activities)
+
 
 def get_binary_file_downloader_html(bin_file, file_label='File'):
     with open(bin_file, 'rb') as f:
@@ -17,14 +19,14 @@ def get_binary_file_downloader_html(bin_file, file_label='File'):
     href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
     return href
 def annotate(name):
-    certi = cv2.imread("C:\\Users\\Admin\Desktop\\github\\certificates_streamlit\\pics\\certificate.jpg")
-    original = cv2.putText(certi, name, (380, 300),font,   fontScale, (0, 0, 0), thickness=2)
-    cv2.imwrite("original.jpg",original)
+    certi = cv2.imread("pics\\img.png")
+    original = cv2.putText(certi, name, (710, 790),font,   fontScale, (0, 0, 0), thickness=5)
+    cv2.imwrite("Certificate.jpg",original)
 
 
     if st.button("View certificate"):
         st.image(original, caption=None, width=350, use_column_width=None, clamp=False, channels='BGR',output_format='PNG')
-        st.markdown(get_binary_file_downloader_html('original.jpg', 'Certificate'), unsafe_allow_html=True)
+        st.markdown(get_binary_file_downloader_html('Certificate.jpg', 'Certificate'), unsafe_allow_html=True)
 
 if choice =="Get certificate":
     st.title("Get Your Certificate")
