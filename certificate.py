d1b9e3b2e99bcf5d950f17cc733233efd9ed67a3
Beatrice <beatrice.federici123@gmail.com>
2022-07-17 15:55:19 +0000

    updaed certifciate.py path files
    updated path files as requested by the comment in my previous pr

--- certificate.py
+++ certificate.py
@@ -6,8 +6,9 @@ import streamlit as st
 font = cv2.FONT_HERSHEY_SIMPLEX
 fontScale = 3
 activities = ["Get certificate","About"]
-cwd = os.getcwd()
-st.sidebar.image(os.path.join(cwd, "images","logo.png"))
+
+
+st.sidebar.image("images/logo.png")
 choice=st.sidebar.selectbox("Select Activty",activities)
 
 
@@ -18,7 +19,7 @@ def get_binary_file_downloader_html(bin_file, file_label='File'):
     href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
     return href
 def annotate(name):
-    certi = cv2.imread(os.path.join(cwd,"images","img.png"))
+    certi = cv2.imread("images/img.png")
     original = cv2.putText(certi, name, (710, 790),font,   fontScale, (0, 0, 0), thickness=5)
     cv2.imwrite("Certificate.jpg",original)
 
@@ -29,7 +30,7 @@ def annotate(name):
 
 if choice =="Get certificate":
     st.title("Get Your Certificate")
-    img=cv2.imread(os.path.join(cwd, "images","apricus.png"))
+    img=cv2.imread("images/apricus.png")
     na=st.text_input('Enter your name')
     annotate(na)
     #st.markdown(get_binary_file_downloader_html(original, 'Picture'), unsafe_allow_html=True)
