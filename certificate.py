38a230d0b41efb7578f76ae7c8ce34fbc6fdff8e
Gunda Sudarrshan <grsudarshan9859@gmail.com>
2022-07-19 16:40:48 +0530

    added new feature (certificate options) to the code

--- certificate.py
+++ certificate.py
@@ -2,6 +2,7 @@ import cv2
 import os
 import base64
 import streamlit as st
+from st_click_detector import click_detector
 import numpy
 import sys
 from PIL import Image, ImageDraw
@@ -18,19 +19,41 @@ def get_binary_file_downloader_html(bin_file, file_label='File'):
     bin_str = base64.b64encode(data).decode()
     href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
     return href
+
 def annotate(name):
-    certi = cv2.imread("images\\img.png")
-    original = cv2.putText(certi, name, (710, 790),font,   fontScale, (0, 0, 0), thickness=5)
+    # this line of code is used to display the Images on the site which are clickable
+    st.write("Select Below Templates")
+    content = """
+                    <a id="Image_1" href="#"><img src="https://i.ibb.co/5WvJgFQ/new-Certificate-template-1.png" alt="new-Certificate-template-1" border="0" width="300px" aspect-ratio="16:9" ></a>
+
+                    <a id="Image_2" href="#"><img src="https://i.ibb.co/CmhrK9d/new-Certificate-template-2.png" alt="new-Certificate-template-2" border="0" width="300px" aspect-ratio="16:9" ></a>
+            """
+    # we store the Id of the clicked image in Clicked Variable 
+    clicked = click_detector(content)
+    # st.markdown(f"**{clicked} clicked**" )
+
+    # the path will be default to the defualt image
+    path = "images\\img.png"
+
+    # we cross check id with the paths available and assign the path for the selected image
+
+    if(clicked == "Image_1"):
+        path = "certificate_templates\\new_Certificate-template-1.png"
+    if (clicked == "Image_2"):
+        path = "certificate_templates\\new_Certificate-template-2.png"
+
+    certi = cv2.imread(path)
+    original = cv2.putText(certi, name, (600, 790),font,   fontScale, (0, 0, 0), thickness=5)
     cv2.imwrite("Certificate.jpg",original)
 
-
+    
     if st.button("View certificate"):
         st.image(original, caption=None, width=350, use_column_width=None, clamp=False, channels='BGR',output_format='PNG')
         st.markdown(get_binary_file_downloader_html('Certificate.jpg', 'Certificate'), unsafe_allow_html=True)
 
+
 if choice =="Get certificate":
     st.title("Get Your Certificate")
-    img=cv2.imread('C:\\Users\\Admin\Desktop\\github\\certificates_streamlit\\pics\\apricus.png')
     na=st.text_input('Enter your name')
     annotate(na)
     #st.markdown(get_binary_file_downloader_html(original, 'Picture'), unsafe_allow_html=True)
@@ -40,4 +63,3 @@ if choice =="About":
     st.subheader("Cerficate App")
     st.markdown("</> with ❤ by Anirudh Soni" )
     st.markdown("connect with me 😃 (https://www.linkedin.com/in/heyanirudh)")
-
