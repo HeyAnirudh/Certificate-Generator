800d7a41f3ca0c02ac51e0f417b32bfd892516ef
Gunda Sudarrshan <grsudarshan9859@gmail.com>
2022-08-17 12:33:49 +0530

    updated file

---
+++ sample.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from st_clickable_images import clickable_images
+
+clicked = clickable_images(
+    [
+        "https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=700",
+        "https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=700",
+        "https://images.unsplash.com/photo-1582550945154-66ea8fff25e1?w=700",
+        "https://images.unsplash.com/photo-1591797442444-039f23ddcc14?w=700",
+        "https://images.unsplash.com/photo-1518727818782-ed5341dbd476?w=700",
+    ],
+    titles=[f"Image #{str(i)}" for i in range(5)],
+    div_style={"display": "flex", "justify-content": "center", "flex-wrap": "wrap"},
+    img_style={"margin": "5px", "height": "200px"},
+)
+
+st.markdown(f"Image #{clicked} clicked" if clicked > -1 else "No image clicked")
